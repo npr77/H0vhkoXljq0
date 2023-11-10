7e9e8ddb49d1315082a8e2c4bfdbb458f5ff1cc6
Nick Prosch <mail@nick-prosch.de>
2023-11-10 11:52:15 +0100

    1:13:20

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,6 +1,6 @@
 const StatusDisplay = () => {
   return (
-    <span className="inline-block rounded-full px-2 px-1 text-xs font-semibold text-gray-700 bg-green-200">
+    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
       done
     </span>
   );
