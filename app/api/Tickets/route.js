e072b249f732294249b28f8b70b8ab05ba99080d
Nick Prosch <mail@nick-prosch.de>
2023-11-22 14:48:09 +0100

    1:33:40

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    const body = await req.json;
-    const ticketData = body.fornData;
+    const body = await req.json();
+    const ticketData = body.formData;
     await Ticket.create(ticketData);
     return NextResponse.json({ message: "Ticket created" }, { status: 201 });
   } catch (error) {
